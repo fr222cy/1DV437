012fcbd62611aed2a9659f15c5405eb1d6bd4d1c
fr222cy <fr222cy@student.lnu.se>
2015-11-11 11:48:30 +0100

    comments

---
+++ Laboration-1-AS-4/Game1/Game1/View/BallView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Game1
+{
+    class BallView
+    {
+
+        Camera camera;
+
+        public BallView()
+        {
+            Camera camera = new Camera();
+        }
+
+        public void Draw(GameTime gametime, SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
+        {
+
+
+
+        }
+
+    }
+
+}
