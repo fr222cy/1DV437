b3893e4532600f8372e40670cf8e55d430aef366
fr222cy <fr222cy@student.lnu.se>
2015-11-19 16:00:48 +0100

    Assignment 1 LAB 2 Completed.

--- Laboration-2/Application.cs
+++ Laboration-2/Application.cs
@@ -1,82 +1,92 @@
-﻿using Game1.Model;
+﻿
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Game1
+namespace Laboration2_AS1
 {
-    
-    
 
-    public class MasterController : Game
+    public class Application : Game
     {
-        BallView bv;
-        BallSimulation bs;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D ball;
-        Texture2D gameArea;
-        public MasterController()
-        {           
+        Texture2D particle;
+        SplitterSystem splitterSystem;
+        private bool SplitterIsStarted = false;
+        public Application()
+        {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-
-            graphics.PreferredBackBufferWidth = 640;
-            graphics.PreferredBackBufferHeight = 480;
-     
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.PreferredBackBufferWidth = 500;
         }
 
-        
+     
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            
-            
+
             base.Initialize();
         }
 
-        //http://stackoverflow.com/questions/5751732/draw-rectangle-in-xna-using-spritebatch
+     
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            ball = Content.Load<Texture2D>("ball");
-            gameArea = new Texture2D(GraphicsDevice, 1, 1);
-            gameArea.SetData(new[] { Color.Yellow });
+            particle = Content.Load<Texture2D>("spark.png");
 
-            
-            bv = new BallView(GraphicsDevice.Viewport);
-            bs = new BallSimulation();
-           
+          
+            splitterSystem = new SplitterSystem();
+        
             // TODO: use this.Content to load your game content here
         }
 
-     
+       
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-       
+   
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
+                
 
-            bs.ballMovement((float)gameTime.ElapsedGameTime.TotalSeconds);
+            // TODO: Add your update logic here
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.D1))
+            {
+                
+                splitterSystem.start(GraphicsDevice.Viewport);
+                SplitterIsStarted = true;
+            }
+            
+            if(SplitterIsStarted)
+            {
+                splitterSystem.particleMovement((float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            
+
             base.Update(gameTime);
         }
 
-
-    
+       
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            bv.drawGameArea(gameArea, spriteBatch);
-            bv.Draw(gameTime, spriteBatch, graphics, ball, bs);
+            if(SplitterIsStarted)
+            {
+                splitterSystem.draw(particle, spriteBatch);
+            }
+            
+            // TODO: Add your drawing code here
+
             base.Draw(gameTime);
         }
-
     }
 }
---
+++ Laboration-2/Camera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Laboration2_AS1
+{
+    class Camera
+    {
+        private float scaleX;
+        private float scaleY;
+        private float scale;
+
+        public Camera(Viewport port)
+        {
+            scaleX = port.Width;
+            scaleY = port.Height;
+
+  
+            if (scaleX < scaleY)
+            {
+                scale = scaleX;
+            }
+            else
+            {
+                scale = scaleY;
+            }
+        }
+
+        public Vector2 modelToViewCoords(Vector2 position)
+        {
+            int screenX = (int)(scale * position.X );
+            int screenY = (int)(scale * position.Y );
+          
+            return new Vector2(screenX, screenY);
+        }
+
+        public float getScale(float size)
+        {
+            return scale * size;
+        }
+       
+    }
+}
