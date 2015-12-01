98208b4bddb2946b4cbceae825133381eafa03e9
fr222cy <fr222cy@student.lnu.se>
2015-12-01 16:44:26 +0100

    Implemented MouseAim.cs

---
+++ Laboration-3/Laboration-3/View/MouseAim.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Laboration_3.Model;
+
+
+namespace Laboration_3.View
+{
+    class MouseAim
+    {
+        Camera camera;
+        Vector2 position;
+       
+        public MouseAim(Viewport port)
+        {
+            camera = new Camera(port);
+        }
+
+        public void update(Vector2 position)
+        {
+            this.position = camera.getClickModelCoords(position) ;
+        }
+
+        public void draw(Texture2D texture, SpriteBatch spriteBatch, BallSimulation ballSimulation)
+        {
+            spriteBatch.Begin();
+
+            float scale = camera.getScale(ballSimulation.getHitRadius(),texture.Width * 1.15f);
+
+            spriteBatch.Draw(texture, camera.getViewCoords(this.position, texture.Width, texture.Height), texture.Bounds, Color.White, 0f,Vector2.Zero, scale, SpriteEffects.None, 0);
+
+            spriteBatch.End();
+        }
+
+        public Vector2 shootPosition()
+        {       
+            return this.position;
+        }
+
+        
+        
+
+        
+
+    }
+}
